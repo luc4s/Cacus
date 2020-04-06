2852d7497e080fd5d66281e714e2340d043c6ee9
Lucas <luca.monnin@gmail.com>
2020-04-06 13:24:19 +0200

    Added GLM and GLFW as submodules

--- src/cacus.cpp
+++ src/cacus.cpp
@@ -2,6 +2,16 @@
 
 #include <iostream>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 Cacus::Cacus() {
-	std::cout << "Bonjour tout le monde" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+	std::cout << "Bonjour tout le monde -> " << test[0] << std::endl;
 }
